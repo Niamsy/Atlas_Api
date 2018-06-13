32487038a927c747185a05fbf2f96f7c3817d01b
menett_a <nicolas1.menettrier@epitech.eu>
2018-06-13 23:44:36 +0200

    [FIX] indentation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const crypto         = require('crypto');
 const SHA256         = require("crypto-js/sha256");
 
 const TokenGenerator = require('uuid-token-generator');
-var hub            = require('hub');
+var hub              = require('hub');
 
 const tokgen = new TokenGenerator();
 
