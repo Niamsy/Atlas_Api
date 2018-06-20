18bf8af3aa6766c702d9bcda4a2db22c43d83e96
My-Lan Aragon <my-lan.aragon@epitech.eu>
2018-06-21 01:34:35 +0200

    [ADD] new routes /plantInfo and /userPlants

--- routes/userPlants.js
+++ routes/userPlants.js
@@ -5,7 +5,6 @@ const con    = require('../index.js').con;
 
 router.get('/', (req, res) => {
     const token = req.header('api_token');
-
     if (!token) {
         res.status(400);
         res.json({message: "Header values are incorrect"});
@@ -13,17 +12,14 @@ router.get('/', (req, res) => {
         res.status(401);
         res.json({message: "Api token is wrong"});
     } else {
-        con.query("SELECT plants.name, plants.scientific_name, plants.maxheight, plants.ids_reproduction, plants.ids_soil_type, plants.ids_soil_ph, plants.ids_soil_humidity, " +
-            "plants.ids_sun_exposure, plants.ids_plant_container, plants.planting_period, plants.florering_period, " +
-            "plants.harvest_period, plants.harvest_period, plants.cutting_period, plants.fk_id_frozen_tolerance," +
-            " plants.fk_id_growth_rate, scanned_at FROM plants INNER JOIN users_plants ON plants.id=users_plants.fk_id_plant" +
+        con.query("SELECT plants.id, scanned_at FROM plants INNER JOIN users_plants ON plants.id=users_plants.fk_id_plant" +
             " where fk_id_user="+ hub.connectedUserToken[token]).then(result => {
             res.status(200);
             res.json(result[0]);
         }).catch(err => {
             res.status(500);
             res.json({message: "Api encountered an issue: +" + err});
-        })
+        });
     }
 });
 
