c8c60aa7c0bf8a67d76f591ef1cb2f6fed944c03
nicolas1.menettrier@epitech.eu <nicolas1.menettrier@epitech.eu>
2018-07-22 22:11:11 +0200

    [ADD] update password

---
+++ routes/updatePassword.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const con    = require('../index.js').con;
+const SHA256 = require("crypto-js/sha256");
+
+let hub      = require('hub');
+
+router.post('/', (req, res) => {
+    const b = req.body;
+
+    if (!b['api_token'] || !b['old_password'] || !b['new_password']) {
+        res.status(400);
+        res.json({message: "Header values are incorrect"});
+    } else if (hub.connectedUserToken[b["api_token"]] == null) {
+        res.status(401);
+        res.json({message: "Api token is wrong"});
+    } else {
+      con.query("SELECT * from users WHERE password=" + con.escape(SHA256(b.old_password).toString()))
+      .then(resu => {
+        if (resu[0].length == 0) {
+          res.status(400).json({message: "wrong password"});
+          return ;
+        }
+      }).catch(e => {
+        res.status(500).json({message: "server error"});
+        return ;
+      });
+      if (b['new_password'].length < 8) {
+        res.status(400).json({message: "password need to have at least 8 characters"});
+      } else {
+        con.query("UPDATE users SET password=" + con.escape(SHA256(b.new_password).toString())
+        + " WHERE password=" + con.escape(SHA256(b.old_password).toString())).then(resu => {
+          res.status(200).json({message: "success"});
+        }).catch(e => {
+          res.status(500).json({message: "server error"});
+        });
+      }
+    }
+});
+
+module.exports = router;
