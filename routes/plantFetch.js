3dbe6da8aab507be0785ba477896af6c2694c525
My Lan Aragon <my-lan.aragon@epitech.eu>
2018-09-21 08:42:33 +0200

    [ADD] growth_duration in route concerning plants

--- routes/plantFetch.js
+++ routes/plantFetch.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
         con.query("SELECT plants.name, plants.scientific_name, plants.maxheight, plants.ids_reproduction, plants.ids_soil_type, plants.ids_soil_ph, plants.ids_soil_humidity, " +
             "plants.ids_sun_exposure, plants.ids_plant_container, plants.planting_period, plants.florering_period, " +
             "plants.harvest_period, plants.harvest_period, plants.cutting_period, plants.fk_id_frozen_tolerance," +
-            " plants.fk_id_growth_rate, scanned_at FROM plants INNER JOIN users_plants ON plants.id=users_plants.fk_id_plant" +
+            " plants.fk_id_growth_rate, scanned_at, growth_duration FROM plants INNER JOIN users_plants ON plants.id=users_plants.fk_id_plant" +
             " where fk_id_user="+ hub.connectedUserToken[token]).then(result => {
             res.status(200);
             res.json(result[0]);
--- routes/plantInfo.js
+++ routes/plantInfo.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
         con.query("SELECT name, scientific_name, maxheight, ids_reproduction, ids_soil_type, ids_soil_ph, plants.ids_soil_humidity, " +
             "ids_sun_exposure, ids_plant_container, planting_period, florering_period, " +
             "harvest_period, harvest_period, cutting_period, fk_id_frozen_tolerance," +
-            " fk_id_growth_rate FROM plants where id=" + plant_id).then(result => {
+            " fk_id_growth_rate, growth_duration FROM plants where id=" + plant_id).then(result => {
                 if (result[0].length <= 0) {
                     res.status(404);
                     res.json({message: "The plant requested doesn't exist"});
