ad2c3b329e4a3f76a579c11b5a12f463d8aa13f9
My Lan Aragon <my-lan.aragon@epitech.eu>
2018-09-30 11:12:13 +0200

    [ADD] test is_admin

--- routes/isAdmin.js
+++ routes/isAdmin.js
@@ -3,21 +3,22 @@ const con = require('../index.js').con;
 
 let hub = require('hub');
 
-router.post('/', function(req, res) {
-  if (req.headers['api_token'] === undefined || req.headers['username'] === undefined) {
-    res.status(400).json({message: 'Need all values in header. (username, api_token).'});
-  } else if (hub.connectedUserToken === undefined || hub.connectedUserToken[req.headers['api_token']] === undefined) {
+router.get('/', function(req, res) {
+  const api_token = req.headers['api_token'];
+  if (api_token === undefined) {
+    res.status(400).json({message: 'Need all values in header (api_token).'});
+  } else if (hub.connectedUserToken === undefined || hub.connectedUserToken[api_token] === undefined) {
       res.status(401).json({message: 'Api token is wrong.'});
   } else {
-    con.query('SELECT rights.name FROM rights, users WHERE rights.id = users.right_id AND users.name = ' + con.escape(req.headers['username']))
+    con.query('SELECT rights.name FROM rights, users WHERE rights.id = users.right_id AND users.id = ' + hub.connectedUserToken[api_token])
     .then(result => {
       if (result[0].length > 0) {
         res.status(200).json({isAdmin: result[0].name === 'admin'});
       } else {
-        res.status(500).json({message: 'No user with username : ' + req.headers['username']})
+        res.status(500).json({message: 'Internal server error: Unknown user'});
       }
     }).catch(err => {
-      res.status(500).json({message: 'Internal server error'});
+      res.status(500).json({message: 'Internal server error:' + err});
       throw err;
     });
   }
---
+++ test/is_admin.js
@@ -0,0 +1,61 @@
+process.env.NODE_ENV = 'test';
+
+const chai     = require('chai');
+const chaiHttp = require('chai-http');
+const server   = require('../index').app;
+const should   = chai.should();
+
+chai.use(chaiHttp);
+
+describe('/GET isAdmin', () => {
+    let api_token;
+
+    before(function (done) {
+        chai.request(server)
+            .post('/user/authentication')
+            .set('username', 'admin')
+            .set('password', 'admin')
+            .end((err, res) => {
+                api_token = res.body.api_token;
+                done();
+            });
+    });
+
+    it('it should returns bad header values', (done) => {
+        chai.request(server)
+            .get('/user/isAdmin')
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message');
+                res.body.message.should.equal("Need all values in header (api_token).");
+                done();
+            });
+    });
+
+    it('it should returns bad token', (done) => {
+        chai.request(server)
+            .get('/user/isAdmin')
+            .set('api_token', "test")
+            .end((err, res) => {
+                res.should.have.status(401);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message');
+                res.body.message.should.equal("Api token is wrong.");
+                done();
+            });
+    });
+
+    it('it should return is admin true', (done) => {
+       chai.request(server)
+           .get('/user/isAdmin')
+           .set('api_token', api_token)
+           .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('isAdmin');
+                res.body.isAdmin.should.equal(true);
+                done();
+           });
+    });
+});
