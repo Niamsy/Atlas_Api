54e618a8625c5902b0a55e9a34eb1e9e241b1f03
Timothée Puentes <timothee.puentes@epitech.eu>
2018-10-03 18:35:34 +0900

    TMP

--- routes/fetchRequestNewPlant.js
+++ routes/fetchRequestNewPlant.js
@@ -13,7 +13,12 @@ router.get('/', (req, res) =>{
     else
     {
         const id = hub.connectedUserToken[token];
-        con.query("SELECT * FROM plant_requests WHERE fk_id_user = " + id)
+        con.query("SELECT id, created_at, name, scientific_name, max_height, "
+            + " ids_soil_ph, ids_soil_type, ids_sun_exposure, ids_soil_humidity,"
+            + " ids_reproduction, ids_plant_container, planting_period, florering_period,"
+            + " harvest_period, cutting_period,"
+            + " fk_id_frozen_tolerance, fk_id_growth_rate"
+            + " FROM plant_requests WHERE fk_id_user = " + id)
             .then(result => {
             res.status(200);
             res.json(result[0]);
