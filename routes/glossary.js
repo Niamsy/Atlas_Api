e534b0e9cc392079f1e3f1e3640ea70d702ba433
Myriam Cholet <myriam.cholet@epitech.eu>
2018-10-07 11:05:27 +0200

    [ADD] more information in the response

--- routes/glossary.js
+++ routes/glossary.js
@@ -10,8 +10,11 @@ router.get('/', (req, res) => {
     } else if (hub.connectedUserToken === undefined || hub.connectedUserToken[api_token] === undefined) {
         res.status(401).json({message: "Api token is wrong"});
     } else {
-        con.query("SELECT plants.name, plants.scientific_name FROM plants INNER JOIN users_plants ON plants.id=users_plants.fk_id_plant" +
-        " where fk_id_user="+ hub.connectedUserToken[api_token]+ "  GROUP BY plants.name")
+        con.query("SELECT plants.name, plants.scientific_name, plants.maxheight, plants.ids_reproduction, plants.ids_soil_type, plants.ids_soil_ph, plants.ids_soil_humidity, " +
+        "plants.ids_sun_exposure, plants.ids_plant_container, plants.planting_period, plants.florering_period, " +
+        "plants.harvest_period, plants.harvest_period, plants.cutting_period, plants.fk_id_frozen_tolerance," +
+        " plants.fk_id_growth_rate, growth_duration FROM plants INNER JOIN users_plants ON plants.id=users_plants.fk_id_plant" +
+        " where fk_id_user="+ hub.connectedUserToken[api_token]+ " GROUP BY plants.name")
         .then(result => {
             if (result[0].length > 0) {
                 res.status(200).json(result[0]);
