67a4563a8537d7c03447a134498af287f912f867
Arnaud Pinta <arnaud.pinta@epitech.eu>
2018-10-12 22:38:14 +0200

    [ADD] models

---
+++ models/Rights.js
@@ -0,0 +1,20 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('rights', {
+    id: {
+      type: DataTypes.INTEGER(4),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      field: 'id'
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      field: 'name'
+    }
+  }, {
+    tableName: 'rights'
+  });
+};
