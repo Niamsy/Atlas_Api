42cf53c767a66eefffce8f1b6b0101cb42025318
Timothée Puentes <timothee.puentes@epitech.eu>
2018-10-17 17:41:41 +0900

    push

--- test/plant/get/fetchRequestNewPlant.js
+++ test/plant/get/fetchRequestNewPlant.js
@@ -7,18 +7,30 @@ const server = require('../../../index').app;
 chai.use(chaiHttp);
 
 describe('/GET /plant/request/fetch', () => {
-  let api_token;
+    let default_api_token;
+    let admin_api_token;
 
   before(done => {
-    chai
-      .request(server)
-      .post('/user/authentication')
-      .set('username', 'admin')
-      .set('password', 'admin')
-      .end((err, res) => {
-        api_token = res.body.api_token;
-        done();
-      });
+      chai
+        .request(server)
+        .post('/user/authentication')
+        .set('username', 'admin')
+        .set('password', 'admin')
+        .end((err, res) => {
+            default_api_token = res.body.api_token;
+            done();
+        });
+  });
+  before(done => {
+      chai
+        .request(server)
+        .post('/user/authentication')
+        .set('username', 'default')
+        .set('password', 'default')
+        .end((err, res) => {
+            admin_api_token = res.body.api_token;
+            done();
+        });
   });
 
   it('It should return Token null', done => {
@@ -26,24 +38,22 @@ describe('/GET /plant/request/fetch', () => {
       .request(server)
       .get('/plant/request/fetch')
       .end((err, res) => {
-        res.should.have.status(400);
+        res.should.have.status(401);
         res.body.should.be.a('object');
         res.body.should.have.property('message');
-        res.body.message.should.equal('Header values are incorrect.');
         done();
       });
   });
 
-  it('It should return Token invalid', done => {
+  it('It should return Not a admin', done => {
     chai
       .request(server)
       .get('/plant/request/fetch')
-      .set('api_token', 'Test')
+      .set('api_token', default_api_token)
       .end((err, res) => {
-        res.should.have.status(401);
+        res.should.have.status(402);
         res.body.should.be.a('object');
         res.body.should.have.property('message');
-        res.body.message.should.equal('Api token is wrong.');
         done();
       });
   });
@@ -52,7 +62,7 @@ describe('/GET /plant/request/fetch', () => {
     chai
       .request(server)
       .get('/plant/request/fetch')
-      .set('api_token', api_token)
+      .set('api_token', admin_api_token)
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
