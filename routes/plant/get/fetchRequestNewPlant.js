4d017a62fd4985aca27507159e786772962bf9fc
Timothée Puentes <timothee.puentes@epitech.eu>
2018-10-24 22:09:52 +0900

    [FIX] Eslint

--- routes/plant/get/fetchRequestNewPlant.js
+++ routes/plant/get/fetchRequestNewPlant.js
@@ -3,29 +3,25 @@ const hub = require('hub');
 const { con } = require('../../../index.js');
 
 router.get('/', async (req, res, next) => {
-    const { api_token } = req.headers;
+  const { apiToken } = req.headers;
 
-    try {
-        const result = await con.query(`SELECT rights.name
+  try {
+    const result = await con.query(`SELECT rights.name
                                            FROM rights,
                                             users
                                             WHERE rights.id = users.right_id
-                                            AND users.id = ${hub.connectedUserToken[api_token]}`
-                                            );
-        if (result[0].length > 0 && result[0][0].name == 'admin')
-        {
-            const result = await con.query(
-                'SELECT id, created_at, name, scientific_name, max_height, ' +
-                ' ids_soil_ph, ids_soil_type, ids_sun_exposure, ids_soil_humidity,' +
-                ' ids_reproduction, ids_plant_container, planting_period, florering_period,' +
-                ' harvest_period, cutting_period,' +
-                ' fk_id_frozen_tolerance, fk_id_growth_rate' +
-                ' FROM plant_requests WHERE status = 1'
-            );
-            res.status(200).json({ requests: result[0] });
-        }
-        else
-            res.status(402).json({ message : "Is not a admin" });
+                                            AND users.id = ${hub.connectedUserToken[apiToken]}`);
+    if (result[0].length > 0 && result[0][0].name === 'admin') {
+      const finalResult = await con.query(
+        'SELECT id, created_at, name, scientific_name, max_height, ' +
+          ' ids_soil_ph, ids_soil_type, ids_sun_exposure, ids_soil_humidity,' +
+          ' ids_reproduction, ids_plant_container, planting_period, florering_period,' +
+          ' harvest_period, cutting_period,' +
+          ' fk_id_frozen_tolerance, fk_id_growth_rate' +
+          ' FROM plant_requests WHERE status = 1'
+      );
+      res.status(200).json({ requests: finalResult[0] });
+    } else res.status(402).json({ message: 'Is not a admin' });
   } catch (err) {
     next(err);
   }
--- test/plant/get/fetchRequestNewPlant.js
+++ test/plant/get/fetchRequestNewPlant.js
@@ -7,30 +7,30 @@ const server = require('../../../index').app;
 chai.use(chaiHttp);
 
 describe('/GET /plant/request/fetch', () => {
-    let default_api_token;
-    let admin_api_token;
+  let defaultApiToken;
+  let adminApiToken;
 
   before(done => {
-      chai
-        .request(server)
-        .post('/user/authentication')
-        .set('username', 'admin')
-        .set('password', 'admin')
-        .end((err, res) => {
-            admin_api_token = res.body.api_token;
-            done();
-        });
+    chai
+      .request(server)
+      .post('/user/authentication')
+      .set('username', 'admin')
+      .set('password', 'admin')
+      .end((err, res) => {
+        adminApiToken = res.body.api_token;
+        done();
+      });
   });
   before(done => {
-      chai
-        .request(server)
-        .post('/user/authentication')
-        .set('username', 'default')
-        .set('password', 'admin')
-        .end((err, res) => {
-            default_api_token = res.body.api_token;
-            done();
-        });
+    chai
+      .request(server)
+      .post('/user/authentication')
+      .set('username', 'default')
+      .set('password', 'admin')
+      .end((err, res) => {
+        defaultApiToken = res.body.api_token;
+        done();
+      });
   });
 
   it('It should return Token null', done => {
@@ -49,7 +49,7 @@ describe('/GET /plant/request/fetch', () => {
     chai
       .request(server)
       .get('/plant/request/fetch')
-      .set('api_token', default_api_token)
+      .set('api_token', defaultApiToken)
       .end((err, res) => {
         res.should.have.status(401);
         res.body.should.be.a('object');
@@ -62,7 +62,7 @@ describe('/GET /plant/request/fetch', () => {
     chai
       .request(server)
       .get('/plant/request/fetch')
-      .set('api_token', admin_api_token)
+      .set('api_token', adminApiToken)
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
