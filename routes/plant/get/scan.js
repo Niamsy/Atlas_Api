9d0ce645f5ac0a70bab16e87b5b92133678d073d
Niamsy <my-lan.aragon@epitech.eu>
2019-05-10 18:14:29 +0900

    Update scan.js

--- routes/plant/get/scan.js
+++ routes/plant/get/scan.js
@@ -52,7 +52,7 @@ router.post('/', async (req, res, next) => {
     const result = await con.query(`SELECT *
               from plants where scientific_name = ${con.escape(scientificName)}`);
     if (result[0].length > 0) {
-       res.send.json({ scientificName });
+       res.status(200).json({ scientificName });
     } else {
       res.status(404).json({ message: 'Plant not found in our database.' });
     }
