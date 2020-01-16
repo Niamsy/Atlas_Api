3b336ec6cd516559e8a2336c322890a42b55eb8d
Puentes <timothee.puentes@epitech.eu>
2020-01-16 17:58:32 +0100

    Update scan.js

--- routes/plant/get/scan.js
+++ routes/plant/get/scan.js
@@ -50,7 +50,7 @@ router.post('/', async (req, res, next) => {
     //if (result[0].length === 0) {
      // res.status(404).json({ message: 'Plant not found in our database.' });
     //} else {
-      res.status(200).json({ "papaver rhoeas" });
+      res.status(200).json({ scientificName : "papaver rhoeas" });
     //}
   //} catch (err) {
      // res.status(500).json({ message: 'Api encountered an issue.' });
